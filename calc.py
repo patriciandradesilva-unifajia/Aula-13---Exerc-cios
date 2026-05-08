3c432101f8e35d893502fb195b3a4707afa61f58
Patrícia Andrade da Silva <patricia.silva882@al.unieduk.com.br>
2026-05-07 21:40:58 -0300

    operações criadas

---
+++ calc.py
@@ -0,0 +1,24 @@
+def soma (a, b):
+    return (a+b)
+
+def subtracao (a, b):
+    return (a-b)    
+
+def multiplicacao (a, b):
+    return (a*b)
+
+def divisao (a, b):
+    if b == 0:
+        return "Não é possível dividir por zero"
+    else:
+        return (a/b)
+
+def potencia (a, b):
+    return (a**b)
+
+def raiz (a, b):
+    if a < 0:
+        return "Não é possível calcular a raiz de um número negativo"
+    else:
+        return (a**0.5)   
+       
